85816d9e27e529f9cc2773c5c0a6f20da422b49a
Kolfie <90023059+Kolfie@users.noreply.github.com>
2022-01-22 17:19:11 +0300

    Add files via upload

---
+++ Wrks2/Wrks2/Wrks2.cpp
@@ -0,0 +1,21 @@
+﻿// Wrks2.cpp
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    const size_t n = 256;
+
+    char a[n];
+    if (!std::cin.getline(a, n)) return 1;
+
+    char b[n];
+    size_t len = std::strlen(a);
+    b[len] = '\0';
+
+    for (size_t i = 0, j = len; j > 0; ++i)
+        b[i] = a[--j];
+
+    std::cout << b << "\n";
+}
